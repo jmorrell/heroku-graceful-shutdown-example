30643057c7fb155fce2ac867ca595409bebc90ce
Jeremy Morrell <jmorrell@heroku.com>
2018-05-24 21:28:55 -0700

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# graceful-shutdown-example
+
+This is a minimal example of capturing the `SIGTERM` signal on Heroku for graceful shutdown
+
+## Deploying
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+Or, from the command line:
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+## Developing locally
+
+```
+$ npm install
+$ npm start
+```
---
+++ src/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const cluster = require('cluster');
+const http = require('http')
+const CONCURRENCY = 4;
+
+// Keep the service alive longer than Heroku allows
+const GRACE = 31;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < CONCURRENCY; i++) {
+    cluster.fork();
+  }
+
+  process.on('SIGTERM', () => {
+    console.log(`Recieved SIGTERM on pid: ${process.pid}`);
+    setTimeout(() => console.log("This should never run"), GRACE);
+  });
+} else {
+  const PORT = process.env.PORT || 5000
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(`Hello, world!\n`)
+  })
+
+  server.listen(PORT, () => console.log(`Listening on ${PORT} on pid: ${process.pid}`))
+
+  process.on('SIGTERM', () => {
+    console.log(`Recieved SIGTERM on pid: ${process.pid}`)
+  });
+}
